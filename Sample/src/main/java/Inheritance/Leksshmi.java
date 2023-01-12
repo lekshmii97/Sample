88ee3583f1684bb8f6d134a7e2a7c0934741f303
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-12 20:52:04 +0530

    commit

---
+++ Sample/src/main/java/Inheritance/Leksshmi.java
@@ -0,0 +1,23 @@
+package Inheritance;
+
+public class Leksshmi
+{
+
+
+
+
+		public void name()
+		{
+			
+		
+		System.out.println("lekshmi she is");
+
+	}
+
+	public static void main(String[] args)
+	{
+		Leksshmi ob=new Leksshmi();
+		ob.name();
+	}
+	}
+
