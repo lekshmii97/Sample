f066a276a7d7487ca8ac55eeadad56dcf09116d6
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-12 12:24:54 +0530

    first commit

---
+++ Sample/src/main/java/Inheritance/Inheritance.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Inheritance
+{
+	public void add()
+	{
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) 
+	{
+		Inheritance obj=new Inheritance();
+        obj.add();
+	}
+}
+
+
