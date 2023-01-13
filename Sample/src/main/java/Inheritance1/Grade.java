4dedbdfe671b2703bc4825dc1284e4a3928bd7ec
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-13 11:21:04 +0530

    Fourth commit

---
+++ Sample/src/main/java/Inheritance1/Grade.java
@@ -0,0 +1,14 @@
+package Inheritance1;
+
+public class Grade 
+{
+public void name()
+{
+	System.out.println("Moto");
+}
+public static void main(String[] args)
+{
+	Grade ob1=new Grade();
+	ob1.name();
+}
+}
