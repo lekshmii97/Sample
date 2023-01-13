6a225aad9eebe6d3da87848089b6dce9fc9a9777
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-13 11:29:33 +0530

    fourth

---
+++ Sample/src/main/java/Inheritance1/Grade1.java
@@ -0,0 +1,18 @@
+package Inheritance1;
+
+
+
+public class Grade1 
+{
+public void name()
+{
+	System.out.println("Moto");
+}
+public static void main(String[] args)
+{
+	Grade1 ob1=new Grade1();
+	ob1.name();
+}
+}
+
+
